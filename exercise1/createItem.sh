613b50bf7e4a495e59f6da650674cf1f6dbb6a5e
bhanukaManesha <bhanukamgamage@gmail.com>
2017-08-26 11:54:29 +0530

    Broken down into functions

--- exercise1/createItem.sh
+++ exercise1/createItem.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+addItem(){
+    
+    echo "-------------" >> items.txt
+    echo "Item ID :"$1 >> items.txt
+    echo "Item Name :"$2 >> items.txt
+    echo "Item Price :"$3 >> items.txt
+}
+
 clear
 echo "-----------------------------"
 echo "Please fill the information"
+
 read -p "Item ID: " ID
 read -p "Item Name: " name
 read -p "Item Price: " price
 
-echo "-------------" >> items.txt
-echo "Item ID :"$ID >> items.txt
-echo "Item Name :"$name >> items.txt
-echo "Item Price :"$price >> items.txt
+addItem $ID $name $price
 
 clear
 echo "Your entry has been saved"
--- exercise1/dashboard.sh
+++ exercise1/dashboard.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 clear
 
-while true :
-do
-echo ""
-echo "*****************************"
-echo ""
-echo "--- WELCOME $1 ---"
-echo ""
-echo "1 - Create Item"
-echo "2 - View Items"
-echo "q - Quit Program"
-read -p "Please enter your choice: " opt
+getUserInput(){
+    while true :
+    do
+    echo ""
+    echo "*****************************"
+    echo ""
+    echo "--- WELCOME $1 ---"
+    echo ""
+    echo "1 - Create Item"
+    echo "2 - View Items"
+    echo "q - Quit Program"
+    read -p "Please enter your choice: " opt
 
-    case "$opt" in
-        "1") ./createItem.sh
-        ;;
-        "2") ./viewItems.sh
-        ;;
-        "q") exit
-        ;;
-    esac
-done
+        case "$opt" in
+            "1") ./createItem.sh
+            ;;
+            "2") ./viewItems.sh
+            ;;
+            "q") exit
+            ;;
+        esac
+    done
+
+}
+
+if [[ ( $1 == "BHANUKA" ) ]]
+then
+    getUserInput $1
+else
+    echo "Please enter ./login.sh <username> <password>"
+fi
+
